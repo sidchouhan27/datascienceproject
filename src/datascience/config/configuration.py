6b5120f8b05f2eb83f0de61f004813d67c288a61
Siddharth Singh Chouhan <siddharthchouhan27@gmail.com>
2025-06-16 23:59:54 -0400

    Model Evaluation and Prediction Pipeline app.py

--- src/datascience/config/configuration.py
+++ src/datascience/config/configuration.py
@@ -2,8 +2,8 @@ from src.datascience.constants import *
 from src.datascience.utils.common import read_yaml, create_directories
 
 from src.datascience.entity.config_entity import (DataIngestionConfig,DataValidationConfig,
-                                                   DataTransformationConfig,ModelTrainerConfig)
-                                                #   ModelEvaluationConfig
+                                                   DataTransformationConfig,ModelTrainerConfig,
+                                                   ModelEvaluationConfig)
 
 class ConfigurationManager:
     def __init__(self,
@@ -74,22 +74,22 @@ class ConfigurationManager:
 
         return model_trainer_config
     
-    # def get_model_evaluation_config(self) -> ModelEvaluationConfig:
-    #     config=self.config.model_evaluation
-    #     params=self.params.ElasticNet
-    #     schema=self.schema.TARGET_COLUMN
+    def get_model_evaluation_config(self) -> ModelEvaluationConfig:
+        config=self.config.model_evaluation
+        params=self.params.ElasticNet
+        schema=self.schema.TARGET_COLUMN
 
-    #     create_directories([config.root_dir])
+        create_directories([config.root_dir])
 
-    #     model_evaluation_config=ModelEvaluationConfig(
-    #         root_dir=config.root_dir,
-    #         test_data_path=config.test_data_path,
-    #         model_path = config.model_path,
-    #         all_params=params,
-    #         metric_file_name = config.metric_file_name,
-    #         target_column = schema.name,
-    #         mlflow_uri="https://dagshub.com/krishnaik06/datascienceproject.mlflow"
+        model_evaluation_config=ModelEvaluationConfig(
+            root_dir=config.root_dir,
+            test_data_path=config.test_data_path,
+            model_path = config.model_path,
+            all_params=params,
+            metric_file_name = config.metric_file_name,
+            target_column = schema.name,
+            mlflow_uri="https://dagshub.com/chouhash/datascienceproject.mlflow"
 
 
-    #     )
-    #     return model_evaluation_config
+        )
+        return model_evaluation_config
